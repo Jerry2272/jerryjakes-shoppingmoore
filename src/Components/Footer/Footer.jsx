fbf7026c62fc0213ff641c6eea24318011a3e7ad
Jerry <jerryjake227@gmail.com>
2024-02-08 22:46:53 +0100

    Add files via upload

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Footer.css'
+import {FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
+
+export const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="container">
+        <div className="iconFooter">
+          <FaFacebook />
+          <FaTwitter />
+          <FaLinkedin />
+          <FaInstagram />
+        </div>
+        <ul style={{display: "flex", gap: '30px', justifyContent: 'center', margin: 'auto', padding: '20px 0'}}>
+          <NavLink>
+              Home
+          </NavLink>
+          <NavLink>
+           Help/FAQ
+          </NavLink>
+          <NavLink>
+            Services
+          </NavLink>
+          <NavLink>
+           privacy
+          </NavLink>
+          <NavLink>
+           Contact
+          </NavLink>
+        </ul>
+        <hr />
+          <p style={{marginTop: 'auto'}}>copyright &copy; 2024|<b>The JerryJakes Shopping Moore</b>. All right reserved.</p>
+        </div>
+    </div>
+  )
+}
